190249e1317bd6e165e658e23e44498545e9624b
felberto <tobias.felber97@gmail.com>
2019-09-06 15:16:11 +0200

    added print for testing

--- MapReduce/MapReduce1.py
+++ MapReduce/MapReduce1.py
@@ -8,6 +8,7 @@ class MapReducer1(MRJob):
         lineList = line.split(",")
         if 'res_send' in lineList[0]:
             yield math.ceil(lineList[6]/60000)
+            print(math.ceil(lineList[6]/60000))
 
     def reducer(self, key, values):
         pass
