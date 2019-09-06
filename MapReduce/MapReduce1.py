5745cbcc0808d6676ac2d0f0955e8d92103afc8d
felberto <tobias.felber97@gmail.com>
2019-09-06 15:32:56 +0200

    fix 2.0

--- MapReduce/MapReduce1.py
+++ MapReduce/MapReduce1.py
@@ -5,13 +5,12 @@ from mrjob.job import MRJob
 
 class MapReduce1(MRJob):
     def mapper(self, _, line):
-        lineList = line.split(",")
-        if 'res_send' in lineList[0]:
+        lineList = line.split(',')
+        if lineList[0] == 'res_send':
             yield math.ceil(int(lineList[6]) / 60000.0), 1
 
-
-def reducer(self, key, values):
-    yield key, sum(values)
+    def reducer(self, key, values):
+        yield key, sum(values)
 
 
 if __name__ == '__main__':
