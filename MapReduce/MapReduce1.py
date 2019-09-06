d929b6b6825e49e67ebb50723b35ab9768de528d
felberto <tobias.felber97@gmail.com>
2019-09-06 15:04:07 +0200

    finished mapper

--- MapReduce/MapReduce1.py
+++ MapReduce/MapReduce1.py
@@ -1,1 +1,17 @@
+import math
+
+from mrjob.job import MRJob
+
+
 class MapReducer1(MRJob):
+    def mapper(self, _, line):
+        lineList = line.split(",")
+        if 'res_send' in lineList[0]:
+            yield math.ceil(lineList[6]/60000)
+
+    def reducer(self, key, values):
+        pass
+
+
+if __name__ == '__main__':
+    MapReducer1.run()
