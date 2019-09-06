79e050ffa8d89751df5e5094ceb0867b9e9c8b86
felberto <tobias.felber97@gmail.com>
2019-09-06 17:26:06 +0200

    test

--- MapReduce/MapReduce1.py
+++ MapReduce/MapReduce1.py
@@ -1,14 +1,17 @@
-import math
-
 from mrjob.job import MRJob
+import math
 
 
 class MapReduce1(MRJob):
+
     def mapper(self, _, line):
-        lineList = line.split(',')
-        if 'res_send' in lineList[0]:
-            print(math.ceil(int(lineList[6]) / 60000.0), 1)
-            yield math.ceil(int(lineList[6]) / 60000.0), 1
+        list = line.split(",")
+        if (list[0] == "res_snd"):
+            yield math.ceil(int(list[
+                                    6]) / 60000.0), 1  # Runden, damit von 1.n bis 2.0 alles als Minute 2 gilt. Damit wird die Minute als Periode betrachtet.
+
+    def combiner(self, key, values):
+        yield key, sum(values)  # Vorverarbeitung für Reducer
 
     def reducer(self, key, values):
         yield key, sum(values)
