9e26d36b6f17db36a912af3ba968882dc97a56e4
felberto <tobias.felber97@gmail.com>
2019-09-06 15:30:57 +0200

    fixed

--- MapReduce/MapReduce1.py
+++ MapReduce/MapReduce1.py
@@ -3,15 +3,16 @@ import math
 from mrjob.job import MRJob
 
 
-class MapReducer1(MRJob):
+class MapReduce1(MRJob):
     def mapper(self, _, line):
         lineList = line.split(",")
         if 'res_send' in lineList[0]:
-            yield math.ceil(int(lineList[6])/60000.0), 1
+            yield math.ceil(int(lineList[6]) / 60000.0), 1
 
-    def reducer(self, key, values):
-        yield key, sum(values)
+
+def reducer(self, key, values):
+    yield key, sum(values)
 
 
 if __name__ == '__main__':
-    MapReducer1.run()
+    MapReduce1.run()
