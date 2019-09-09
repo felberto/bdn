0755fbf1321ea55c3b031e358cdbbac5b337e8e3
felberto <tobias.felber97@gmail.com>
2019-09-09 16:05:28 +0200

    fixed steps

--- MapReduce/MapReduce2.py
+++ MapReduce/MapReduce2.py
@@ -1,14 +1,24 @@
 import math
 
 from mrjob.job import MRJob
+from mrjob.step import MRStep
 
 
 class MapReduce2(MRJob):
 
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper,
+                   reducer=self.reducer1),
+            MRStep(combiner=self.combiner,
+                   reducer=self.reducer2)
+        ]
+
     def mapper(self, _, line):
         lineList = line.split(',')
-        if lineList[1] != 'client_id' and lineList[1] != -1 and lineList[1] != 0:
-            yield str(lineList[1]) + ':' + str(lineList[2]), int(lineList[6])
+        if lineList[0] == 'msg_snd' and lineList[0] == 'res_rcv':
+            if lineList[1] != -1 and lineList[1] != 0:
+                yield str(lineList[1]) + ':' + str(lineList[2]), int(lineList[6])
 
     def reducer1(self, key, values):
         values = list(values)
