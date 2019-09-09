9f971ba7d6ead450603312b78a0e63dcd5d0c3eb
felberto <tobias.felber97@gmail.com>
2019-09-09 14:53:59 +0200

    added combiner

--- MapReduce/MapReduce1.py
+++ MapReduce/MapReduce1.py
@@ -7,7 +7,10 @@ class MapReduce1(MRJob):
     def mapper(self, _, line):
         lineList = line.split(',')
         if 'res_snd' in lineList[0]:
-            yield math.ceil(int(lineList[6]) / 60000.0), 1
+            yield math.ceil(int(lineList[6]) / 60000), 1
+
+    def combiner(self, key, values):
+        yield key, sum(values)
 
     def reducer(self, key, values):
         yield key, sum(values)
